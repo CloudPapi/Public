9b5f392266e2d89c0cb70a6bc043f9b28700af82
smaskefar <sijola@outlook.dk>
2022-11-15 07:21:27 +0100

    Create Recurse-AzureADAdminSQL.ps1

---
+++ Recurse-AzureADAdminSQL.ps1
@@ -0,0 +1,9 @@
+﻿Connect-AzAccount | Out-Null
+$servers = Get-AzSqlServer
+$PrincipalDisplayName = ""
+foreach($server in $servers){
+    $AdminID = (Get-AzSqlServer -ServerName $server.ServerName).Administrators.Login
+    if($AdminID -ne $PrincipalDisplayName){
+        Set-AzSqlServerActiveDirectoryAdministrator -DisplayName $PrincipalDisplayName -ServerName $($server.ServerName) -ResourceGroupName $server.ResourceGroupName
+    }
+}
